2f0f756937505a74e6fdb8917a37d8241e9088f8
June <adasilva.html@gmail.com>
2022-12-28 13:08:48 -0500

    Finish html structure
    finished the structure i want for html.
    Now its ready to be erased and have index.js recreate it with javascript

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,8 +5,8 @@ const { merge } = require("webpack-merge");
 module.exports = merge(common, {
   mode: "production",
   output: {
-    filename: "main.[contenthash].js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+    // clean: true,
   },
 });
