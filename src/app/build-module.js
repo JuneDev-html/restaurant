5819bc884ace9592d94c9079012228ab6b1a902c
June <adasilva.html@gmail.com>
2022-12-30 13:31:37 -0500

    Add footer links

--- src/app/build-module.js
+++ src/app/build-module.js
@@ -1,9 +1,7 @@
 export const buildElement = (el_type, class_name, content) => {
   const element = document.createElement(el_type);
   if (class_name) element.classList.add(class_name);
-  if (content) element.innerText = content;
+  if (content) element.innerHTML = content;
 
   return element;
 };
-
-// ---------- FIX SPACING OF FOOTER ON HOME
--- src/app/footer.js
+++ src/app/footer.js
@@ -3,15 +3,19 @@ import { buildElement } from "./build-module";
 export const buildFooter = () => {
   const footer = buildElement("footer", null, null);
   const footer_par = buildElement("p", "footer-par", "--Built by JuneDev");
+  const git_img = buildElement("img", "git-img", null);
   const git_link = buildElement("a", "logos", null);
-  // git_link.href = "https://github.com/JuneDev-html";
+  git_link.append(git_img);
+  git_link.href = "https://github.com/JuneDev-html";
 
   const yt_link = buildElement("a", "logos", null);
-  // yt_link.href = "https://www.youtube.com/channel/UC8ryMqGJPToOXdwj4IqwE-Q";
+  const yt_img = buildElement("img", "yt-img", null);
+  yt_link.append(yt_img);
+  yt_link.href = "https://www.youtube.com/channel/UC8ryMqGJPToOXdwj4IqwE-Q";
 
   footer.append(footer_par);
   footer.append(git_link);
-  footer.append(git_link);
+  footer.append(yt_link);
 
   return footer;
 };
